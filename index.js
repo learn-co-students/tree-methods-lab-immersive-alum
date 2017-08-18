3ad525f9b70c9e77a3ec05e1fe043b1606f97a1d
github username <github email address>
2017-08-18 10:44:32 -0400

    three tests passing

--- index.js
+++ index.js
@@ -6,5 +6,25 @@ function inOrder(currentNode){
   if (currentNode.right){
     inOrder(currentNode.right)
   }
+}
 
+function findOrAdd(currentNode, newNode){
+  if (currentNode == newNode.data){
+    return true
+  }
+  if (newNode.data < currentNode.data){
+    if (currentNode.left){
+      return findOrAdd(currentNode.left, newNode)
+    } else {
+      return currentNode.left = newNode
+    }
+  }
+
+  if (newNode.data > currentNode.data){
+    if (currentNode.right){
+      return findOrAdd(currentNode.right, newNode)
+    } else {
+      return currentNode.right = newNode
+    }
+  }
 }
