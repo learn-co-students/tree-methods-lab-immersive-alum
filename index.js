743ed77dac8a0ff0668c2457d1a341f7770559f0
Brett Schules <brettschules@gmail.com>
2017-10-16 13:31:16 -0400

    Done

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-function max(currentNode) {
-  if (currentNode.right) {
-    return max(currentNode.right)
-  } else {
-    return currentNode;
-  }
-}
-
-function min(currentNode) {
-  if(currentNode.left) {
-    return min(currentNode.left)
-  } else {
-    return currentNode;
-  }
-}
-
 function inOrder(currentNode) {
   if (currentNode.left) {
     inOrder(currentNode.left)
   }
+
   console.log(currentNode.data)
 
-  if(currentNode.right) {
-    inOrder(currentNode.right)
-  }
+  if(currentNode.right){
+   inOrder(currentNode.right)
+ }
 }
 
 function findOrAdd(currentNode, newNode){
@@ -37,9 +22,29 @@ function findOrAdd(currentNode, newNode){
     }
   }
 
-  if(currentNode.right){
-    return findOrAdd(currentNode.right, newNode)
-  } else {
-    return currentNode.right = newNode
+  if(newNode.data > currentNode.data){
+    if(currentNode.right){
+      return findOrAdd(currentNode.right, newNode)
+    } else {
+      return currentNode.right = newNode
+    }
   }
- }
+}
+
+
+function min(currentNode){
+  if(currentNode.left){
+    return min(currentNode.left)
+  } else {
+    return currentNode;
+  }
+}
+
+
+function max(currentNode){
+  if(currentNode.right){
+    return max(currentNode.right)
+  } else {
+    return currentNode;
+  }
+}
